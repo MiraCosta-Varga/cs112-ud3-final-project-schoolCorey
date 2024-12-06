60a57b565ca63166094d67b82b45f0938e81b73f
schoolCorey <moontribedude@gmail.com>
2024-12-07 08:53:25 +0900

    Exception Documentation

--- src/main/java/cs112/ud3/Exceptions/OpponentNotValidException.java
+++ src/main/java/cs112/ud3/Exceptions/OpponentNotValidException.java
@@ -1,5 +1,9 @@
 package cs112.ud3.Exceptions;
 
+/**
+ * Thrown if the user tries to create a DMOpponent that does not actually exist in game,
+ * such as one which has an invalid combination of name and location instance variables.
+ */
 public class OpponentNotValidException extends Exception {
     public static final String DEFAULT_MESSAGE = "Opponent of selected name does not exist at selected location in-game.\nTry double-checking your selections.";
     public OpponentNotValidException(){
--- src/main/java/cs112/ud3/Exceptions/UninitializedLinkException.java
+++ src/main/java/cs112/ud3/Exceptions/UninitializedLinkException.java
@@ -1,5 +1,9 @@
 package cs112.ud3.Exceptions;
 
+/**
+ * Thrown if a ValidLink class tries to call a method that depends on its valid arrays before
+ * createValidArray() is called.
+ */
 public class UninitializedLinkException extends Exception{
     public static final String DEFAULT_MESSAGE = "Valid array not yet created. Please call createValidArray() before checking for validity of objects.";
 
